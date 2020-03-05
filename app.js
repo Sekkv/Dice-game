c828b9606c891a097809853bda880e05566dd03f
sereeke <Sereejidb@gmail.com>
2020-03-06 01:38:27 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -9,18 +9,17 @@ var roundScore = 0;
 
 // shoonii ali talaara buusniig hadgalah huwisagch heregtei , 1-6 gesen utgiig ene huwisagchid sanamsarguigeer uusgej ugnu.
 
-var dice = Math.floor(Math.random() * 6) + 1;
-
-// id = "score-0"
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-
-// id = "score-1"
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
 //document.querySelector("#score-1").innerHTML = "<em>Yes!</em>";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-window.document.querySelector(".dice").style.display = "none";
-
-console.log("shoo : " + dice);
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
